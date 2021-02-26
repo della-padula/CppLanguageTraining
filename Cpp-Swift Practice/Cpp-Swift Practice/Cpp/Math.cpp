4c8c5a77d54589a58005610e52277db1b021b7ca
denny.k <denny.k@kakaocorp.com>
2021-02-26 19:47:01 +0900

    C++ Swift Practice Project Created

---
+++ Cpp-Swift Practice/Cpp-Swift Practice/Cpp/Math.cpp
@@ -0,0 +1,9 @@
+//
+//  Math.cpp
+//  Cpp-Swift Practice
+//
+//  Created by denny on 2021/02/26.
+//
+
+#include "Math.hpp"
+#include <iostream>
---
+++ Cpp-Swift Practice/Cpp-Swift Practice/Cpp/Math.hpp
@@ -0,0 +1,13 @@
+//
+//  Math.hpp
+//  Cpp-Swift Practice
+//
+//  Created by denny on 2021/02/26.
+//
+
+#ifndef Math_hpp
+#define Math_hpp
+
+#include <stdio.h>
+
+#endif /* Math_hpp */
