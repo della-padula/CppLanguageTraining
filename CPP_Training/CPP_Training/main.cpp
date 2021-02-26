511fcd480b60bf15fce4b4e6c1f3e436373eb564
denny.k <denny.k@kakaocorp.com>
2021-02-26 19:40:16 +0900

    C++ Project Initialization

---
+++ CPP_Training/CPP_Training/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPP_Training
+//
+//  Created by denny on 2021/02/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
