c250648bb9e659bf2d7be540a0a27399f1d1db88
denny.k <denny.k@kakaocorp.com>
2021-03-10 21:50:14 +0900

    Function Templates

--- CPP_Training/CPP_Training/Sample.cpp
+++ CPP_Training/CPP_Training/Sample.cpp
@@ -5,6 +5,42 @@
 //  Created by denny on 2021/02/26.
 //
 
+//#include <iostream>
+//using namespace std;
+//
+//int operate (int a, int b) {
+//    return a * b;
+//}
+//
+//double operate (double a, double b) {
+//    return a / b;
+//}
+//
+//int main () {
+//    int x = 5, y = 2;
+//    double n = 5.0, m = 2.0;
+//    cout << operate (x, y) << '\n';
+//    cout << operate (n, m) << '\n';
+//    return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//#define PI 3.14159
+//#define NEWLINE '\n'
+//
+//int main(void) {
+//    double radius = 5.0;
+//    double circle;
+//
+//    circle = 2 * PI * radius;
+//    cout << circle;
+//    cout << NEWLINE;
+//
+//    return 0;
+//}
+
 #include <iostream>
 using namespace std;
 
--- CPP_Training/CPP_Training/main.cpp
+++ CPP_Training/CPP_Training/main.cpp
@@ -8,35 +8,19 @@
 #include <iostream>
 using namespace std;
 
-int operate (int a, int b) {
-    return a * b;
-}
-
-double operate (double a, double b) {
-    return a / b;
+template <class T>
+T sum (T a, T b) {
+    T result;
+    result = a + b;
+    return result;
 }
 
 int main () {
-    int x = 5, y = 2;
-    double n = 5.0, m = 2.0;
-    cout << operate (x, y) << '\n';
-    cout << operate (n, m) << '\n';
+    int i = 5, j = 6, k;
+    double f = 2.0, g = 0.5, h;
+    k = sum<int>(i, j);
+    h = sum<double>(f, g);
+    cout << k << '\n';
+    cout << h << '\n';
     return 0;
 }
-
-//#include <iostream>
-//using namespace std;
-//
-//#define PI 3.14159
-//#define NEWLINE '\n'
-//
-//int main(void) {
-//    double radius = 5.0;
-//    double circle;
-//
-//    circle = 2 * PI * radius;
-//    cout << circle;
-//    cout << NEWLINE;
-//
-//    return 0;
-//}
