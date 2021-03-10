17bff7f003efe53614047e21159f893eac2cac93
denny.k <denny.k@kakaocorp.com>
2021-03-10 21:48:37 +0900

    Function Overloding

--- CPP_Training/CPP_Training/main.cpp
+++ CPP_Training/CPP_Training/main.cpp
@@ -8,16 +8,35 @@
 #include <iostream>
 using namespace std;
 
-#define PI 3.14159
-#define NEWLINE '\n'
+int operate (int a, int b) {
+    return a * b;
+}
 
-int main(void) {
-    double radius = 5.0;
-    double circle;
-    
-    circle = 2 * PI * radius;
-    cout << circle;
-    cout << NEWLINE;
-    
+double operate (double a, double b) {
+    return a / b;
+}
+
+int main () {
+    int x = 5, y = 2;
+    double n = 5.0, m = 2.0;
+    cout << operate (x, y) << '\n';
+    cout << operate (n, m) << '\n';
     return 0;
 }
+
+//#include <iostream>
+//using namespace std;
+//
+//#define PI 3.14159
+//#define NEWLINE '\n'
+//
+//int main(void) {
+//    double radius = 5.0;
+//    double circle;
+//
+//    circle = 2 * PI * radius;
+//    cout << circle;
+//    cout << NEWLINE;
+//
+//    return 0;
+//}
